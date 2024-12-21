187d71d4fda77b7354866e32e90584652895ca6e
ayush577 <agandhi62@gmail.com>
2024-12-21 21:49:10 +0530

    add pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      script: "npm run start",
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: "key.pem",
+      user: "ubuntu",
+      host: "54.208.48.94",
+      ref: "origin/main",
+      repo: "git@github.com:ayush577/aws-next.git",
+      path: "/home/ubuntu",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
